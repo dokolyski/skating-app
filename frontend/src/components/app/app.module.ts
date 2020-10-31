9658c425677eed1bc0c060ef836605317e184b2a
dokolyski <dominikkalinowski15@wp.pl>
2020-10-31 18:03:06 +0100

    [added] few new dependencies, templates for scheduler component, pages folder removed

--- frontend/src/components/app/app.module.ts
+++ frontend/src/components/app/app.module.ts
@@ -5,19 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from 'environments/environment';
-import { NewBookingComponent } from 'components/pages/new-booking/new-booking.component';
+import { NewBookingComponent } from 'components/new-booking/new-booking.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule, MatIconModule, MatTabsModule, MatToolbarModule} from '@angular/material';
+import {SessionCardComponent} from '../session-card/session-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NewBookingComponent
+    NewBookingComponent,
+    SessionCardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
-    BrowserAnimationsModule
+    ServiceWorkerModule.register('ngsw-worker.js', {enabled: environment.production}),
+    BrowserAnimationsModule,
+    MatTabsModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
