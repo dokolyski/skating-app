2faff85b19ef017fd136c5077fb03439ebfaa91e
Korneliusz Szymański <colorgreen19@gmail.com>
2020-12-02 22:41:07 +0100

    Add handling sequelize validation errors

--- backend/src/middlewares/validate-errors-middleware.ts
+++ backend/src/middlewares/validate-errors-middleware.ts
@@ -1,20 +1,36 @@
 import {ValidatorError} from 'typescript-class-validator';
 import {Request, Response} from "express";
 import HttpCode from 'http-status-codes'
+import sequelize from "sequelize";
 
 export function ValidateRequestMiddleware() {
     return (error, req: Request, res: Response, next) => {
         if (error instanceof ValidatorError) {
             res.status(HttpCode.UNPROCESSABLE_ENTITY).json(mapErrors(error));
+        } else if (error instanceof sequelize.ValidationError) {
+            res.status(HttpCode.UNPROCESSABLE_ENTITY).json(mapSequelizeErrors(error));
+        } else {
+            res.status(HttpCode.INTERNAL_SERVER_ERROR).json({
+                error: error.message
+            });
         }
     }
 }
 
 function mapErrors(error: ValidatorError) {
-    return error.validationErrors.map( e => {
+    return error.validationErrors.map(e => {
         return {
             name: e.property,
             errors: e.constraints
+        }
+    });
+}
+
+function mapSequelizeErrors(error: sequelize.ValidationError) {
+    return error.errors.map(e => {
+        return {
+            name: e.path,
+            errors: e.message
         }
     });
 }
