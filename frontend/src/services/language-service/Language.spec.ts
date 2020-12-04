a35f3ddb98bc2ddf7968864262902c1eefed4b36
adam <razzors1998@gmail.com>
2020-12-04 17:17:06 +0100

    Moved tests data into assets

--- frontend/src/services/language-service/Language.spec.ts
+++ frontend/src/services/language-service/Language.spec.ts
@@ -1,12 +1,11 @@
 import { TestBed } from "@angular/core/testing"
-import { LanguageService } from "./Language.service"
-import * as LANG_DEFAULT from './default-test.language.json'
-import * as LANG_CUSTOM from './custom-test.language.json'
 import { filter, mergeMap } from "rxjs/operators"
+import { LanguageService } from "./Language.service"
+import * as LANG_DEFAULT from 'assets/mocks/unit-tests/language-service/default-test.language.json'
+import * as LANG_CUSTOM from 'assets/mocks/unit-tests/language-service/custom-test.language.json';
+import { custom_lang, default_lang } from 'assets/mocks/unit-tests/language-service/config.json';
 
 describe('language.service', () => {
-    const default_lang = 'default-test'
-    const custom_lang = 'custom-test'
     let languageService: LanguageService
 
     beforeEach(async (done: DoneFn) => {
@@ -14,7 +13,7 @@ describe('language.service', () => {
             providers: [
                 LanguageService,
                 { provide: 'language', useValue: default_lang },
-                { provide: 'path-languages', useValue: 'services/language-service' }
+                { provide: 'path-languages', useValue: 'assets/mocks/unit-tests/language-service' }
             ]
         }).compileComponents()
         
