436f9b53b25897132e38024f9d34a12aab9b9dfa
Korneliusz Szymański <colorgreen19@gmail.com>
2020-12-27 22:21:47 +0100

    Add joining to session

--- backend/src/controllers/api/session_participant/session_participant.ts
+++ backend/src/controllers/api/session_participant/session_participant.ts
@@ -1,10 +1,18 @@
 import express, {Request, Response} from 'express'
+import HttpCode from "http-status-codes";
+import SessionsParticipants from "../../../services/sessions-participants";
 
 const router = express.Router()
 
 router
-    .post('/session_participant', async (req: Request, res: Response) => {
-
+    .post('/session_participants', async (req: Request, res: Response, next) => {
+        SessionsParticipants.join(req.body, req.user.id)
+            .then(result => {
+                res.status(HttpCode.OK).send(result);
+            })
+            .catch(e => {
+                next(e)
+            });
     })
 
 export default router
