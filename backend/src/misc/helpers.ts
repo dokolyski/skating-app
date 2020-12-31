cb619601369fae85fd1c295ddeda52f1d9635a0b
Korneliusz Szymański <colorgreen19@gmail.com>
2020-12-31 01:23:22 +0100

    Add toNumber helper function

--- backend/src/misc/helpers.ts
+++ backend/src/misc/helpers.ts
@@ -1,6 +1,14 @@
 import NotFoundException from "./not-found-exception";
+import BadRequestException from "./bad-request-exception";
 
-export function notfound(object) {
-    if( object === null )
+export function notfound(object): void {
+    if (object === null)
         throw new NotFoundException()
+}
+
+export function toNumber(value: string): number {
+    const v = parseInt(value);
+    if (isNaN(v))
+        throw new BadRequestException()
+    return v
 }
