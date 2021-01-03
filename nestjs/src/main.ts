bf81146b2b269e758343c87d1d66d677845a62a8
Korneliusz Szymański <colorgreen19@gmail.com>
2021-01-03 11:09:04 +0100

    Add swagger

--- nestjs/src/main.ts
+++ nestjs/src/main.ts
@@ -1,20 +1,22 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import {SwaggerModule, DocumentBuilder} from '@nestjs/swagger';
 import ValidationPipe from './validation-pipe'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule);
+    app.setGlobalPrefix('api')
     app.useGlobalPipes(ValidationPipe)
 
-  const options = new DocumentBuilder()
-      .setTitle('Backend')
-      .setDescription('Backend description')
-      .setVersion('1.0')
-      .build();
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api', app, document);
+    const options = new DocumentBuilder()
+        .setTitle('Backend')
+        .setDescription('Backend description')
+        .setVersion('1.0.1')
+        .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api', app, document);
 
-  await app.listen(80);
+    await app.listen(80);
 }
+
 bootstrap();
