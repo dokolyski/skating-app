cd101dff88561048402ff18b5acedc5601492a41
Korneliusz Szymański <colorgreen19@gmail.com>
2021-01-03 11:08:23 +0100

    Add validation pipeline

--- nestjs/src/main.ts
+++ nestjs/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import ValidationPipe from './validation-pipe'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+    app.useGlobalPipes(ValidationPipe)
 
   const options = new DocumentBuilder()
       .setTitle('Backend')
