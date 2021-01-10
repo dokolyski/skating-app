a0dc22dcbb9520b5e96b1bc786779286ac4361ee
Korneliusz Szymański <colorgreen19@gmail.com>
2021-01-10 01:21:43 +0100

    Add responses

--- backend/src/api/responses/profile.dto.ts
+++ backend/src/api/responses/profile.dto.ts
@@ -1,20 +1,12 @@
-
 export class ProfileResponse {
 
     id: number;
-
     user_id: number;
-
     is_owner: boolean;
-
     firstname: string;
     lastname: string;
     birth_date: Date;
-
     skill_level: string;
-
-    // @Column({})
-    //  readonly createdAt: Date;
-    // @Column({})
-    //  readonly updatedAt: Date
+    createdAt: Date;
+    updatedAt: Date
 }
--- backend/src/api/responses/session.dto.ts
+++ backend/src/api/responses/session.dto.ts
@@ -1,5 +1,19 @@
+import {UserSimplifiedResponse} from "./user.dto";
+import {ProfileResponse} from "./profile.dto";
 
-export default class SessionResponse
-{
+export default class SessionResponse {
 
+    id: number
+    owner: UserSimplifiedResponse;
+    profiles: ProfileResponse[];
+    name: string
+    start_date: Date
+    end_date: Date
+    max_participants: number
+    difficulty: string
+    price: number
+    description: string
+    status: string
+    createdAt: Date
+    updatedAt: Date
 }
