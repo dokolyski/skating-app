cb4e9acc8bb3083251a29c77ad8d1576e69a1e17
dominikkalinowski <ESacwmnw0404>
2021-01-16 14:57:11 +0100

    [added] common user directive

---
+++ frontend/src/directives/access-control/app-access-common-user.directive.ts
@@ -0,0 +1,28 @@
+import { Directive, OnDestroy, TemplateRef, ViewContainerRef } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { AuthService } from 'services/auth-service/Auth.service';
+
+@Directive({
+  selector: '[appAccessCommonUser]'
+})
+export class AppAccessCommonUserDirective implements OnDestroy {
+  private subs = this.auth.sessionInfo$
+    .pipe(
+      map(v => !v.isAdmin && !v.isHAdmin && !v.isOrganizer)
+    ).subscribe(ok => {
+      if (ok) {
+        this.container.createEmbeddedView(this.el);
+      } else {
+        this.container.clear();
+      }
+    });
+
+  constructor(
+    private el: TemplateRef<any>,
+    private container: ViewContainerRef,
+    private auth: AuthService) { }
+
+  ngOnDestroy() {
+    this.subs.unsubscribe();
+  }
+}
