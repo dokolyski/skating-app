7481a3f36bb78eb462f56a32a0fb07b43e0a1312
dominikkalinowski <ESacwmnw0404>
2021-01-23 14:10:16 +0100

    [WIP] loading fb-page href dynamically (but whole page isn't loading)

--- frontend/src/components/pages/main-page/news-tab/news-tab.component.ts
+++ frontend/src/components/pages/main-page/news-tab/news-tab.component.ts
@@ -1,5 +1,9 @@
-import { AfterViewInit, Component } from '@angular/core';
-import { NewsService } from 'services/news-service/news.service';
+import {AfterViewInit, Component} from '@angular/core';
+import {NewsService} from 'services/news-service/news.service';
+import {RestService} from 'services/rest-service/rest.service';
+import {ConfigResponse} from 'api/responses/config.dto';
+import * as REST_CONFIG from 'assets/config/config.rest.json';
+import * as REST_PATH from 'api/rest-url.json';
 
 @Component({
   selector: 'app-news-tab',
@@ -7,11 +11,18 @@ import { NewsService } from 'services/news-service/news.service';
   styleUrls: ['./news-tab.component.css']
 })
 export class NewsTabComponent implements AfterViewInit {
-  constructor(private news: NewsService) {
+
+  fbPageAddress: string;
+
+  constructor(private news: NewsService,
+              private restService: RestService) {
     this.news.fetchNews();
   }
 
   ngAfterViewInit() {
-    this.news.fetchNews();
+    this.restService.do<ConfigResponse>(REST_PATH.CONFIG.GET, {templateParamsValues: {key: REST_CONFIG.fb_link}}).subscribe(next => {
+      this.fbPageAddress = next.value;
+      this.news.fetchNews();
+    });
   }
 }
