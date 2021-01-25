90570994bc8d2e90f5950a3130c35257410124bf
dominikkalinowski <ESacwmnw0404>
2021-01-25 17:06:58 +0100

    [improved] about us loaded from assets, with full text open on click

--- frontend/src/components/pages/main-page/about-us-tab/about-us-tab.component.ts
+++ frontend/src/components/pages/main-page/about-us-tab/about-us-tab.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import * as ABOUT_US from 'assets/content/about-us.json';
+import {FormatterService} from 'services/formatter-service/formatter.service';
+import {MatDialog} from '@angular/material/dialog';
+import {FullArticleComponent} from 'components/pages/main-page/about-us-tab/full-article/full-article.component';
 
 @Component({
   selector: 'app-about-us-tab',
   templateUrl: './about-us-tab.component.html',
   styleUrls: ['./about-us-tab.component.css']
 })
-export class AboutUsTabComponent { }
+export class AboutUsTabComponent {
+  articles = ABOUT_US['default'];
+  maxArticleLength = 250;
+
+  constructor(public formatterService: FormatterService,
+              public dialog: MatDialog){
+  }
+
+  showFullArticle(article: any, img_href: string) {
+    this.dialog.open(FullArticleComponent, { data: {article, img_href} });
+  }
+}
--- frontend/src/components/pages/main-page/about-us-tab/about-us-tab.module.ts
+++ frontend/src/components/pages/main-page/about-us-tab/about-us-tab.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AboutUsTabComponent } from './about-us-tab.component';
+import { FullArticleComponent } from './full-article/full-article.component';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatButtonModule} from '@angular/material/button';
+import {TranslateModule} from '@ngx-translate/core';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatDialogModule,
+    MatButtonModule,
+    TranslateModule
   ],
-  declarations: [AboutUsTabComponent],
+  declarations: [AboutUsTabComponent, FullArticleComponent],
   exports: [AboutUsTabComponent]
 })
 export class AboutUsTabModule { }
